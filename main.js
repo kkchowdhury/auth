08dc023da7d0245078fed780073ea628d7a97039
KISHALOY KAR CHOWDHURY <107786775+kkchowdhury@users.noreply.github.com>
2024-04-05 16:04:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,12 @@
-import firebase from 'firebase/app';
-import { datadogRum } from '@datadog/browser-rum';
-datadogRum.init({
-    applicationId: '5497fe57-a64c-4dda-8e73-999b2c3a8452',
-    clientToken: 'pub33f8f2d18197e40c6753e04645049746',
-    site: 'us5.datadoghq.com',
-    service: 'auth', 
-    sessionSampleRate: 100,
-    sessionReplaySampleRate: 80,
-    trackUserInteractions: true,
-    trackResources: true,
-    trackLongTasks: true,
-    defaultPrivacyLevel: 'allow',
-});
-
-
 // setting up firebase with our website
-const firebaseConfig = {
+const firebaseApp = firebase.initializeApp({
     apiKey: "AIzaSyDqzAnc8GWylzz917YuYwhLarbKku8LE2o",
     authDomain: "auth-f7501.firebaseapp.com",
     projectId: "auth-f7501",
     storageBucket: "auth-f7501.appspot.com",
     messagingSenderId: "943960156531",
     appId: "1:943960156531:web:c73716d97220860951fd06"
-};
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+});
 const db = firebaseApp.firestore();
 const auth = firebaseApp.auth();
 
@@ -38,9 +21,6 @@ const signUp = () => {
             // Signed in 
             document.write("You are Signed Up")
             console.log(result)
-            datadogRum.setUser({
-                email: email
-            });
             // ...
         })
         .catch((error) => {
@@ -60,9 +40,6 @@ const signIn = () => {
             // Signed in 
             document.write("You are Signed In")
             console.log(result)
-             datadogRum.setUser({
-                email: email
-            });
         })
         .catch((error) => {
             console.log(error.code);
